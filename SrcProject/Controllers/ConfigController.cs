80410ba04a868be2245d332eeeee2b57966593c7
Carlos Carmona <carloscarmona0829@gmail.com>
2025-06-09 19:08:25 -0500

    feat(ConfigController): Add ChangeLogo

---
+++ SrcProject/Controllers/ConfigController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SrcProject.Controllers
+{
+    [Route("Config")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("ChangeLogo")]
+        public async Task<ActionResult> PostChangeLogo(IFormFile file)
+        {
+            try
+            {
+                var response = await Utilities.Utils.ChangeImage(file, _configuration["FilePaths:Logo"]);
+
+                return StatusCode(StatusCodes.Status200OK,
+                      new { IsSuccess = true, Message = response.IsSuccess == true ? "Imagen actualizada con éxito" : "No se pudo actualizar la imagen", Result = response });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { IsSuccess = false, Message = "Error interno del servidor.", Result = ex.Message });
+            }
+        }
+
+    }
+}
---
+++ SrcProject/Utilities/Utils.cs
@@ -0,0 +1,67 @@
+﻿namespace SrcProject.Utilities
+{
+    public class Utils
+    {
+        public static async Task<ResponseManager> ChangeImage(IFormFile file, string filePath)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return new ResponseManager
+                {
+                    IsSuccess = false,
+                    Message = "Archivo no válido. ",
+                    Response = null
+                };
+            }
+
+            if(filePath == null || string.IsNullOrEmpty(filePath))
+            {
+                return new ResponseManager
+                {
+                    IsSuccess = false,
+                    Message = "Ruta de archivo no válida. ",
+                    Response = null
+                };
+            }
+
+            var fileName = "Logo.png";
+            var currentFilePath = Path.Combine(filePath, fileName);
+            if (System.IO.File.Exists(currentFilePath))
+            {
+                var timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                var oldFileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+                var oldFileExtension = Path.GetExtension(fileName);
+
+                var newOldFileName = $"{oldFileNameWithoutExtension}_{timestamp}{oldFileExtension}";
+                var newOldFilePath = Path.Combine(filePath, newOldFileName);
+
+                try
+                {
+                    System.IO.File.Move(currentFilePath, newOldFilePath);
+                    LogManager.DebugLog($"Antigua imagen renombrada a: {newOldFileName}");
+                }
+                catch (Exception ex)
+                {
+                    LogManager.DebugLog("Error en el método UploadFile. " + ex.Message);
+                    return new ResponseManager
+                    {
+                        IsSuccess = false,
+                        Message = "Error en el método UploadFile.",
+                        Response = ex.Message
+                    };
+                }
+            }
+
+            using (var stream = new FileStream(currentFilePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return new ResponseManager
+            {
+                IsSuccess = true,
+                Message = "Imagen actualizada exitosamente.",
+                Response = null
+            };
+        }       
+    }
+}
